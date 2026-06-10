a28b4a44b6fc7680d0601e9c80e733b566f47ef1
Roman Matviy <roman@matviy.pp.ua>
2026-06-10 22:41:56 +0300

    feat: generate additional localized static pages and update site generation script

--- generate_seo_pages.py
+++ generate_seo_pages.py
@@ -79,9 +79,14 @@ PAGE_TEMPLATE = """<!DOCTYPE html>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Roboto:wght@300;400;500;700&display=swap" as="style">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Roboto:wght@300;400;500;700&display=swap" media="print" onload="this.media='all'">
+    <noscript>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Roboto:wght@300;400;500;700&display=swap">
+    </noscript>
     
     <!-- Styles -->
+    <link rel="preload" href="/css/style.css?ver=3" as="style">
     <link rel="stylesheet" href="/css/style.css?ver=3">
     
     <!-- Open Graph -->
